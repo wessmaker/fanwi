3ff26ed00d6c61cb5904ec327e014f4ad49f2262
wessmaker <eemi.wessman@gmail.com>
2024-07-28 22:43:45 +0300

    feat: Made enum for communication commands.

--- attiny85_program/attiny85_program/src/drivers/serial.h
+++ attiny85_program/attiny85_program/src/drivers/serial.h
@@ -3,6 +3,15 @@
 #ifndef SERIAL_H
 #define SERIAL_H
 
+enum {   //This enum is used in TX and RX
+   RECEIVE_START = 0xAA,
+   RECEIVE_STOP = 0xFF,
+   SPINNING_START = 0xB1,
+   SPINNING_STOP = 0xB2,
+   TEMPERATURE_DRIVEN_START = 0xB3,
+   TEMPERATURE_DRIVEN_STOP = 0xB4
+};
+
 void initializeSerial();
 void sendByte(uint8_t byte);
 uint8_t readByte();
