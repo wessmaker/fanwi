b22e28ddc28e0d8448a547b1693a91cc10ee5b99
wessmaker <eemi.wessman@gmail.com>
2024-08-04 23:49:45 +0300

    feat: Added I2C inital pin configurations

--- attiny85_program/src/settings/pins.c
+++ attiny85_program/src/settings/pins.c
@@ -2,9 +2,12 @@
 #include "registers.h"
 
 void pins_initialize(void){
-   DDRB |= (1 << 3); //PB4 (pin 2) as output for TX
-   PORTB |= (1 << 3); //Set TX pin high as initial state
-   DDRB |= (0 << 4); //PB3 (pin 3) as input for RX
-   DDRB |= (1 << 0); //PB0 (pin 5) as output for PWM (OC0A)
-   
+   DDRB |= (1 << 3); //PB4 (pin 2) as output TX
+   PORTB |= (1 << PINB3); //Set TX pin high at initial state
+   DDRB |= (0 << 4); //PB3 (pin 3) as RX input 
+   DDRB |= (1 << 0); //PB0 (pin 5) as PWM output (OC0A)
+   DDRB |= (1 << 5);  //PB5 (pin 1) as SDA output !!RESET PIN, SET FUSES!!
+   DDRB |= (1 << 2);  //PB2 (pin 7) as SCL output
+   PORTB |= (1 << SDA_PIN);   //Set I2C pins high at initial state
+   PORTB |= (1 << SCL_PIN);
 };
--- attiny85_program/src/settings/pins.h
+++ attiny85_program/src/settings/pins.h
@@ -1,8 +1,7 @@
 #ifndef PINS_H
 #define PINS_H
-
 void pins_initialize(void);
-
 #define PINB3 3
-
+#define SDA_PIN 5
+#define SCL_PIN 2
 #endif
